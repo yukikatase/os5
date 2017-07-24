fce0b77072ddb5d2479cdc5e1e1a9613861f4b7a
yukikatase <arasiusu@gmail.com>
2017-07-24 21:07:43 +0900

    大体できた
    なんか強制終了する

--- thread.c
+++ thread.c
@@ -72,8 +72,10 @@ int timer_handler() {
   out8(0x20, 0x60);	/* タイマー割り込み (IRQ0) を再度有効にする */
   timer_counter++;
 
-  if (running_thread == idle && blue_ends == 0)
+  if (running_thread == idle && blue_ends == 0 && timer_counter % 4 == 1)
     thread = blue;
+  else if(running_thread == idle && yellow_ends == 0 && timer_counter % 4 == 3)
+    thread = yellow;
   else
     thread = idle;
 
